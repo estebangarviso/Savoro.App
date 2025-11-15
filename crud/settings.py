21b92c3c79d565d1432805696156341e684920f1
Esteban Garviso <e.garvisovenegas@gmail.com>
2025-11-15 02:14:44 -0300

    fix: Update static and media file settings in Django configuration

--- crud/settings.py
+++ crud/settings.py
@@ -116,12 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 # añadir otra dirección donde django buscara archivos estáticos
-STATICFILES_DIRS = (os.path.abspath(os.path.join(BASE_DIR, "../static")),)
+STATICFILES_DIRS = [BASE_DIR + "/static"]
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "../media")
 
 LOGIN_URL = "/"
